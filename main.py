ce58baebbd5bccb2b57fc7996bd3ba2b1d1951b1
czechyc <yuri.c.czech@gmail.com>
2025-01-16 17:21:06 -0300

    B: adds player rotation

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -13,7 +17,12 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        pygame.time.Clock().tick(60)
+        dt = pygame.time.Clock().tick(60) / 1000
 
 if __name__ == "__main__":
     main()
